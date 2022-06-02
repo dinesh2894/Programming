62bbaa11cb6d74e73415834c922c6a28f7f007ff
dinesh <dinesh2894>
2022-06-02 21:00:06 +0530

    Initial commit

---
+++ src/DSA/ArrayDemo.java
@@ -0,0 +1,16 @@
+package DSA;
+
+public class Array {
+
+    public static void main(String[] args){
+       int [] arr = new int[5];
+        System.out.println(arr);
+
+        System.out.println(arr.length);
+
+        // update / set
+        arr[1] = 6;
+
+        System.out.println(arr.);
+    }
+}
